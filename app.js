5012361b81d3ba376cec308e3b15b4fbfde16b4e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-08 22:05:28 +0100

    initialized auth route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 // requiring routes
 const NotFoundError = require('./errors/notFound');
@@ -14,6 +15,8 @@ if (app.get('env') === 'development') {
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -23,6 +26,7 @@ app.use((req, res, next) => {
 // api routes
 app.use('/api/v1/albums', require('./routes/albums'));
 app.use('/api/v1/users', require('./routes/users'));
+app.use('/api/v1/auth', require('./routes/auth'));
 
 app.all('*', (req, res, next) => {
   next(new NotFoundError(`Can't find ${req.originalUrl} on this server`));
