65b79992378c49af5358a1cd05526a51dc5cf22f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-08 17:07:08 +0100

    setup database config

---
+++ config/db.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+require('colors');
+
+dotenv.config({ path: './config.env' });
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { DATABASE, DATABASE_LOCAL, DATABASE_PASSWORD } = process.env;
+
+const dbLocal = DATABASE_LOCAL;
+
+const mongoURI = DATABASE.replace('<PAASWORD>', DATABASE_PASSWORD);
+
+const db = devEnv ? dbLocal : mongoURI;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(db);
+    console.log(`Connected to MongoDB → ${conn.connection.port}`.gray.bold);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
