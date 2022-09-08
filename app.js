f7119d63fb8e19ae52dc13f23726c984ea0048e8
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-08 17:53:43 +0100

    initialized albums route

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// requiring routes
+
 const app = express();
 
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
+
+// test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// api routes
+app.use('/api/v1/albums', require('./routes/albums'));
 
 module.exports = app;
