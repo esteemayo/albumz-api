9aa4e0573d73d8547fde0a673a0f39d459d8d621
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-08 17:26:24 +0100

    created albums model

---
+++ models/Album.js
@@ -0,0 +1,66 @@
+const slugify = require('slugify');
+const mongoose = require('mongoose');
+
+const albumSchema = new mongoose.Schema(
+  {
+    artist: {
+      type: String,
+      required: [true, 'Album must have an artist'],
+    },
+    title: {
+      type: String,
+      required: [true, 'Album must have must a title'],
+    },
+    genre: {
+      type: String,
+      required: [true, 'Album must belong to a genre'],
+    },
+    slug: String,
+    info: {
+      type: String,
+      required: [true, 'Please provide the album info'],
+    },
+    year: {
+      type: String,
+      required: [true, 'Album must have a year of release'],
+    },
+    label: {
+      type: String,
+      required: [true, 'Please tell us your record label'],
+    },
+    tracks: {
+      type: Number,
+      required: [true, 'Please tell us your album number of tracks'],
+    },
+    tags: {
+      type: Array,
+      validate: {
+        validator: function (val) {
+          return val && val.length > 0;
+        },
+        message: 'Album should have at least one tag',
+      },
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+albumSchema.pre('save', async function (next) {
+  if (!this.isModified('title')) return next();
+  this.slug = slugify(this.title, { lower: true });
+
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const albumWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+  if (albumWithSlug.length) {
+    this.slug = `${this.slug}-${albumWithSlug.length + 1}`;
+  }
+});
+
+const Album = mongoose.models.Album || mongoose.model('Album', albumSchema);
+
+module.exports = Album;
