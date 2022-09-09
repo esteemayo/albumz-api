52157d6f6d4377a96db5163fcf18db682e00450e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-09 20:39:57 +0100

    created genre route

---
+++ routes/genre.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const authMiddleware = require('../middlewares/authMiddleware');
+const genreController = require('../controllers/genreController');
+
+const router = express.Router();
+
+router.use(authMiddleware.protect);
+
+router.get('/show/:slug', genreController.getGenreBySlug);
+
+router
+  .route('/')
+  .get(genreController.getGenres)
+  .post(genreController.createGenre);
+
+router
+  .route('/:id')
+  .get(genreController.getGenreById)
+  .patch(genreController.updateGenre)
+  .delete(genreController.deleteGenre);
+
+module.exports = router;
