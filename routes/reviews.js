14b7202e4a69d957310f814f8c141669d5a82ca5
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-09 20:39:33 +0100

    created reviews route

---
+++ routes/reviews.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const reviewController = require('../controllers/reviewController');
+
+const router = express.Router();
+
+router.use(authMiddleware.protect);
+
+router
+  .route('/')
+  .get(reviewController.getReviews)
+  .post(authController.restrictTo('user'), reviewController.createReview);
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(reviewController.updateReview)
+  .delete(reviewController.deleteReview);
+
+module.exports = router;
