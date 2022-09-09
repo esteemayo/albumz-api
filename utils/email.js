a77ad7f67d940eaec11c1bc0d6dbea9b2d50dd6d
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-09 20:40:14 +0100

    send email logic

---
+++ utils/email.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: `Support <${process.env.EMAIL_FROM}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    html: options.html,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
