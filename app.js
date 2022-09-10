4ae4e3b18b3e61c75e88bd35f2e6c6f0c7cfc8bd
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-10 17:44:52 +0100

    implemented CORS

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // requiring routes
 const NotFoundError = require('./errors/notFound');
 const globalErrorHandler = require('./middlewares/errorHandler');
 
+// start express app
 const app = express();
 
+// global middlewares
+
+// implement CORS
+app.use(cors());
+
+// Access-Control-Allow-Origin
+app.options('*', cors());
+
+// set security HTTP headers
+
+// development logging
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit request from same api
+
+// body Parser, reading data from body into req.body
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
+// cookie parser middleware
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// data sanitization against NoSQL query injection
+
+// data sanitization against XSS
+
+// prevent parameter pollution
+
+// compression middleware
 
 // test middleware
 app.use((req, res, next) => {
@@ -23,7 +48,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// api routes
+// api routes middleware
 app.use('/api/v1/albums', require('./routes/albums'));
 app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/auth', require('./routes/auth'));
