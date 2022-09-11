abcf0d69a919f4c25f16b75f30128ab396ccc7ff
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-11 16:01:52 +0100

    bookmark model

---
+++ models/Bookmark.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const bookmarkSchema = new mongoose.Schema(
+  {
+    album: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Album',
+      required: [true, 'A bookmark must belong to an album'],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'A bookmark must belong to a user'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+bookmarkSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'album',
+    select: 'artist title genre info year label',
+  });
+
+  next();
+});
+
+const Bookmark =
+  mongoose.models.Bookmark || mongoose.model('Bookmark', bookmarkSchema);
+
+module.exports = Bookmark;
