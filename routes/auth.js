25cd2bee518e41ea7efb1741468864bb2c9fb93c
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-17 02:37:17 +0100

    added google login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.post('/login', authController.login);
 
+router.post('/google-login', authController.googleLogin);
+
 router.post('/forgot-password', authController.forgotPassword);
 
 router.post('/reset-password/:token', authController.resetPassword);
