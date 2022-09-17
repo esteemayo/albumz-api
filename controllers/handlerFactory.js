e3f35c69aff7bd88477b8e44bdd166e59ca8c000
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-17 02:38:17 +0100

    modified

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -6,7 +6,10 @@ const NotFoundError = require('../errors/notFound');
 
 exports.getAll = (Model) =>
   asyncHandler(async (req, res, next) => {
-    const features = new APIFeatures(Model.find(), req.query)
+    let filter = {};
+    if (req.params.albumId) filter = { album: req.params.albumId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
@@ -22,11 +25,14 @@ exports.getAll = (Model) =>
     });
   });
 
-exports.getOneById = (Model) =>
+exports.getOneById = (Model, popOptions) =>
   asyncHandler(async (req, res, next) => {
     const { id: docId } = req.params;
 
-    const doc = await Model.findById(docId);
+    let query = Model.findById(docId);
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
 
     if (!doc) {
       return next(
@@ -40,11 +46,14 @@ exports.getOneById = (Model) =>
     });
   });
 
-exports.getOneBySlug = (Model) =>
+exports.getOneBySlug = (Model, popOptions) =>
   asyncHandler(async (req, res, next) => {
     const { slug } = req.params;
 
-    const doc = await Model.findOne({ slug });
+    let query = Model.findOne({ slug });
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
 
     if (!doc) {
       return next(
