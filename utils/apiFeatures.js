2e93d4a3b137c529ac55b6897cff0b66cb1dc302
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-15 17:56:56 +0100

    modified

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -43,7 +43,7 @@ class APIFeatures {
   paginate() {
     // pagination
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 10;
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
