925c5d8faaae1f09c2f2c1dc133249b5f3098096
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-12 00:48:46 +0100

    modified

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import authMiddleware from '../middlewares/authMiddleware.js';
-import * as authController from '../controllers/authController.js';
 import * as userController from '../controllers/userController.js';
 
 const router = express.Router();
@@ -12,7 +11,7 @@ router.use(authMiddleware.protect);
 
 router.get(
   '/stats',
-  authController.restrictTo('admin'),
+  authMiddleware.restrictTo('admin'),
   userController.getUserStats
 );
 
@@ -24,13 +23,13 @@ router.delete('/delete-me', userController.deleteMe);
 
 router
   .route('/')
-  .get(authController.restrictTo('admin'), userController.getUsers)
+  .get(authMiddleware.restrictTo('admin'), userController.getUsers)
   .post(userController.createUser);
 
 router
   .route('/:id')
   .get(userController.getUser)
-  .patch(authController.restrictTo('admin'), userController.updateUser)
-  .delete(authController.restrictTo('admin'), userController.deleteUser);
+  .patch(authMiddleware.restrictTo('admin'), userController.updateUser)
+  .delete(authMiddleware.restrictTo('admin'), userController.deleteUser);
 
 export default router;
