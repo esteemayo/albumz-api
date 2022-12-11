81dd10c62c70c5c5018ad068d3958aa23a3e0816
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-12 00:24:54 +0100

    fix: fix a bug

--- errors/notFound.js
+++ errors/notFound.js
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
-import CustomAPIError from './ customAPIError.js';
+import CustomAPIError from './customAPIError.js';
 
 class NotFoundError extends CustomAPIError {
   constructor(message) {
